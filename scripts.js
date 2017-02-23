e393dbb77e7e5052546236fd2e0d52f9a76da2b5
Figlarz <Figlarz@Figlarzs-MacBook-Pro.local>
2017-02-24 00:21:57 +0100

    fixed

--- scripts.js
+++ scripts.js
@@ -7,9 +7,9 @@ var itemsLength = buttonItems.length;
 
 //console.log(itemsLength);
 
-for (var n = 0 ; n < (itemsLength + 1) ; n++) {
+for (var n = 0 ; n < itemsLength ; n++) {
     var buttonSelect = buttonItems[n],
-        buttonText = buttonSelect.textContent; //wyskakuje mi błąd "Cannot read property 'textContent' of undefined" (lub innerText jeśli tym zrobię, ale podobno nie wszystko go czyta więc uzyłem text Content) robiłem to w jednej zmiennej zeby było ładniej, rozbicie na dwie dalej daje undefined nie wiem dlaczego - tak czy inaczej wysylam bo wynik jest poprawny.
+        buttonText = buttonSelect.innerText;
     alert(buttonText);
  //   console.log(buttonText);
 }
